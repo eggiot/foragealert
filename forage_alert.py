3785c00dfa0c26870d4c25aaa5f9bf6f771adf5b
Eliot Walker <eliot@eliotjwalker.com>
2021-01-02 16:35:06 +0000

    Fixing rule lists

--- forage_alert.py
+++ forage_alert.py
@@ -53,12 +53,15 @@ def errorandquit(error_message):
 # Rule class
 class Rule():
     def __init__(self, rule_dict):
-        self.rule_dict = {k: int(v) for (k, v) in rule_dict.items()}
+        self.rule_dict = rule_dict
         # list keys should be a string of comma separated integer values
         list_keys = [key for key in self.rule_dict if "_list" in key]
-        for key in list_keys:
-            value = self.rule_dict[key]
-            self.rule_dict[key] = [int(v) for v in value.split(",")]
+        for (k, v) in self.rule_dict.items():
+            try:
+                rule_dict[k] = int(v)
+            except ValueError:
+                if "_list" in k:
+                    rule_dict[k] = [int(x) for x in v.split(",")]
         self.hours = build_range_list(self.rule_dict, "hour", 0, 23, 23, [])
         self.days = build_range_list(self.rule_dict, "day", 0, 100, 7, [])
         self.pick_hours = build_range_list(self.rule_dict, "pick", 0, 1, 23,
